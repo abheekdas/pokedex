c76422c66402cf2d2ae83d9687030327fea67409
Abheek Das <abheek.das1605@gmail.com>
2021-05-09 09:43:47 +0530

    Initial Commit

---
+++ PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Abheek Das on 09/05/21.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
